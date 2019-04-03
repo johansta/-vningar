c43a6686ba59da931d357a6d9ee82f794a2f11be
Johan Ståhlberg <Johan Sthlberg@DESKTOP-SASC4MU>
2019-04-03 17:03:25 +0200

    Test

---
+++ Övning 3/NumericInputError.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Övning_3
+{
+    class NumericInputError : UserError
+    {
+        public override String UEMessage()
+        {
+            return "You tried to use a text input in a numericonly field.This fired an error!";
+        }
+    }
+}
---
+++ Övning 3/UserError.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Övning_3
+{
+    abstract class UserError
+    {
+        public abstract String UEMessage();
+       
+    }
+}
