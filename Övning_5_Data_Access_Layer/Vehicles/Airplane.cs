dcca963557ef3841188ed0278baf3ec7188ea57a
Johan Ståhlberg <johan.stahlberg.83@gmail.com>
2019-05-02 18:43:38 +0200

    Revert "Fixed bugs"
    This reverts commit 409d5d89c0164c4533160bd952ed871d341d97fd.

--- Övning_5_Data_Access_Layer/Vehicles/Airplane.cs
+++ Övning_5_Data_Access_Layer/Vehicles/Airplane.cs
@@ -12,7 +12,7 @@ namespace Övning_5_Data_Access_Layer.Vehicles
 
         public Airplane(String licence, int numberOfParachutes) : base(licence)
         {
-            NumberOfParachutes = numberOfParachutes;
+            NumberOfParachutes = NumberOfParachutes;
         }
 
         public override String ToString()
